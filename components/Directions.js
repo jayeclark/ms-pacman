4743d0c199edf5c4a0ba08f8bb441ba60cb5b495
Jay Clark <jay@jayeclark.dev>
2021-12-17 16:29:12 -0400

    Destructure props
    Signed-off-by: Jay Clark <jay@jayeclark.dev>

--- components/Directions.js
+++ components/Directions.js
@@ -6,17 +6,16 @@ export class Directions {
     this.down = this.props("down", board);
   }
 
-  props(dir, board) {
-    const { tileW: t, fringeW: f } = board;
-    const row =  /down/.test(dir) ? 1 : /up/.test(dir) ? -1 : 0;
-    const col =  /right/.test(dir) ? 1 : /left/.test(dir) ? -1 : 0;
-    const [r, rY] = [Math.abs(row) * 90, /down|right/.test(dir) ? 0 : 180];
+  props(dir, { tileW: t, fringeW: f, speed }) {
+    const row =  dir === "down" ? 1 : dir === "up" ? -1 : 0;
+    const col =  dir === "right" ? 1 : dir === "left" ? -1 : 0;
+    const [r, rY] = [row == 0 ? 0 : 90, row + col > 0 ? 0 : 180];
     const transform = `rotate(${r}deg) rotateY(${rY}deg)`;
     const reverse = 'updown'.includes(dir) ? 'updown'.replace(dir, '') : 'rightleft'.replace(dir, '');
-    const speed = /up|left/.test(dir) ? -board.speed : board.speed;
-    const eyeLeft = f * (2.5 + col / 2);
+    speed = /up|left/.test(dir) ? -speed : speed;
+    const [eyeTop, eyeLeft] = [t / 6 + f, f * (5 + col) / 2];
     const pupilTop = dir !== 'down' ? t / 6 + f * 2.5 + row * f * 2 : t / 6 + f * 3;
     const pupilLeft = f * (3 + col);
-    return { transform, reverse, row, col, speed,  eyeTop: t / 6 + f, eyeLeft, pupilTop, pupilLeft }
+    return { transform, reverse, row, col, speed, eyeTop, eyeLeft, pupilTop, pupilLeft }
   }
 }
