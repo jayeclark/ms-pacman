27bd5c49526388613f47be70cada10a73eb5bd4f
Jay Clark <jay@jayeclark.dev>
2021-12-22 16:57:15 -0400

    Test commit #5
    Signed-off-by: Jay Clark <jay@jayeclark.dev>

--- tests/components/Arrow.spec.js
+++ tests/components/Arrow.spec.js
@@ -19,7 +19,7 @@ describe(format('Arrow'), () => {
     expect(instance).toBe('Element');
   });
 
-  it('has a constructor that accepts between two and four arguments', () => {
+  it('has a constructor that accepts between two to four arguments', () => {
     expect(sampleArrow.constructor.length).toBe(2);
   });
 
