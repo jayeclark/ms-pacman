7563b4f585d87c91d9ad20cf9b95f3a132070207
Jay Clark <jay@jayeclark.dev>
2022-02-21 20:01:13 -0400

    File location
    Signed-off-by: Jay Clark <jay@jayeclark.dev>

--- data/boards.js
+++ data/boards.js
@@ -1,7 +1,7 @@
 // import board from './board1.json' assert { type: "json" };
 
 export default async function loadBoards() {
-  const response = await fetch('board1.json');
+  const response = await fetch('./data/board1.json');
   const board1raw = await response.text();
   const board1 = await JSON.parse(board1raw);
   return { board1 };
